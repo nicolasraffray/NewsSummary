027f037de95c607cefb04da1291a3889dbeb05aa
nicolasraffray <nraffray@gmail.com>
2020-04-28 12:12:44 +0100

    progress on news summary api call

--- apiData.js
+++ apiData.js
@@ -1,19 +1,36 @@
 
 var request = new XMLHttpRequest()
-var url = "https://content.guardianapis.com/search?show-elements=all&show-fields=all&api-key=f302c6ce-8b45-410f-92dc-9cd452aeb06a&q=uk&page-size=5"
-request.open('GET',url,true)
+var url = ["https://content.guardianapis.com/search?show-elements=all&show-fields=all&api-key=f302c6ce-8b45-410f-92dc-9cd452aeb06a&q=uk&page-size=5", "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=https://www.theguardian.com/world/live/2020/apr/23/coronavirus-live-news-update-covid-19-face-masks"] 
 
 var storyList = new StoryList
 
-request.onload = function(){
-  var data = JSON.parse(this.response).response.results
-  console.log(data)
-  if(request.status === 200){ for(var i = 0; i < data.length; i++ ){
-    storyList.addStory(data[i].webTitle, data[i].webPublicationDate, data[i].webUrl, image = data[i].fields.main)}
+for(let i = 0; i < url.length; i++){
 
-  var controller = new StoryController(storyList)
-  controller.insertHeadlineHTML()}
-  else{ console.log("API Error")}
+  request.open('GET',url[i],true)
+  console.log(request)
+  request.onreadystatechange = function(){
+    console.log(request.readyState)
+    console.log(XMLHttpRequest.DONE)
+    console.log(request.status)
+    if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
+      console.log(request)
+      var data = JSON.parse(request.responseText);
+      console.log('DATA FROM THE API', data.text)
+    } 
+  }
+  request.send()
 }
 
-request.send()
+
+// request.onload = function(){
+//   var data = JSON.parse(this.response).response.results
+//   console.log(data)
+//   if(request.status === 200){ for(var i = 0; i < data.length; i++ ){
+//     storyList.addStory(data[i].webTitle, data[i].webPublicationDate, data[i].webUrl, image = data[i].fields.main)}
+
+//   var controller = new StoryController(storyList)
+//   controller.insertHeadlineHTML()}
+//   else{ console.log("API Error")}
+// }
+
+// request.send()
